fdd6dbe9dcd73d1259d8070d3b134255d96b8fb0
seli0123@stud.kea.dk <seli0123@stud.kea.dk>
2020-01-03 17:53:51 +0100

    Halvvejs med portfolio site

--- script.js
+++ script.js
@@ -2,11 +2,25 @@ window.addEventListener("load", SidenVises);
 
 function SidenVises() {
     console.log("SidenVises");
-    document.querySelector("#menuknap").addEventListener("click", KlikMenu);
 
+
+
+
+    // registrer klik på menu-knappen
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
 }
 
-function KlikMenu() {
-    console.log("KlikMenu");
-    document.querySelector("#menu").classList.remove("hidden");
+function toggleMenu() {
+    console.log("Toggle menu");
+    document.querySelector("#menu").classList.toggle("hidden");
+
+    let erSkjult = document.querySelector("#menu").classList.contains("hidden");
+
+    if (erSkjult == true) {
+        //menuen er nu skjult - ændre menuknappen til burgersymbol
+        document.querySelector("#menuknap").textContent = "☰";
+    } else {
+        //menuen er vist - ændre til X
+        document.querySelector("#menuknap").textContent = "X";
+    }
 }
